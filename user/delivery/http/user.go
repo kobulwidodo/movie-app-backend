3a51937b94856a36d371a2547acd3806b4cd4fb4
kobulwidodo <fadhilhan01@gmail.com>
2022-03-01 15:57:19 +0700

    Add Auth Usecase

--- user/delivery/http/user.go
+++ user/delivery/http/user.go
@@ -11,11 +11,13 @@ import (
 
 type UserHandler struct {
 	UserUsecase domain.UserUsecase
+	AuthUsecase domain.AuthUsecase
 }
 
-func NewUserHandler(r *gin.Engine, uu domain.UserUsecase) {
-	handler := &UserHandler{UserUsecase: uu}
+func NewUserHandler(r *gin.Engine, uu domain.UserUsecase, au domain.AuthUsecase) {
+	handler := &UserHandler{UserUsecase: uu, AuthUsecase: au}
 	r.POST("/register", handler.Register)
+	r.POST("/login", handler.Login)
 }
 
 func (h *UserHandler) Register(c *gin.Context) {
@@ -32,4 +34,27 @@ func (h *UserHandler) Register(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, &util.Response{Data: &entity.GetUser{Name: newUser.Name, Email: newUser.Email, Bio: newUser.Bio}, Message: "successfully created a user"})
+}
+
+func (h *UserHandler) Login(c *gin.Context) {
+	var input entity.LoginInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, &util.Response{Message: err.Error()})
+		return
+	}
+
+	var userLogged domain.User
+	userLogged, err := h.UserUsecase.Login(input)
+	if err != nil {
+		c.JSON(util.GetErrorCode(err), &util.Response{Message: err.Error()})
+		return
+	}
+
+	token, err := h.AuthUsecase.GenerateToken(userLogged.ID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, &util.Response{Message: domain.ErrInternalServerError.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, &util.Response{Data: &entity.GetUser{Name: userLogged.Name, Email: userLogged.Email, Bio: userLogged.Bio, Token: token}, Message: "Success Login"})
 }
