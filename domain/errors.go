a9af6aa81ab7c9571a2f900b5723a2bdf6682c07
kobulwidodo <fadhilhan01@gmail.com>
2022-03-01 15:56:26 +0700

    add 2 error

--- domain/errors.go
+++ domain/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrInternalServerError = errors.New("internal server error")
 	ErrEmailConflict       = errors.New("the email has been already registered")
 	ErrInputBinding        = errors.New("wrong parameter")
+	ErrNotFound            = errors.New("credential not found")
+	ErrPassNotMatch        = errors.New("your login credentials dont match in our system")
 )
