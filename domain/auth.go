1c9f6fbeb7b2edfd9b35f9409d0ed3ed80028bc8
kobulwidodo <fadhilhan01@gmail.com>
2022-03-02 08:14:19 +0700

    Add validate token to interface

--- domain/auth.go
+++ domain/auth.go
@@ -1,5 +1,8 @@
 package domain
 
+import "github.com/golang-jwt/jwt"
+
 type AuthUsecase interface {
 	GenerateToken(userId uint) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
