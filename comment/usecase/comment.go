02850681866fbfa1b3259ee70954d8814f0576a2
kobulwidodo <fadhilhan01@gmail.com>
2022-03-02 15:00:46 +0700

    Finish 2 usecase

--- comment/usecase/comment.go
+++ comment/usecase/comment.go
@@ -1,8 +1,13 @@
 package usecase
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"movie-app/comment/entity"
 	"movie-app/domain"
+	"net/http"
+	"strconv"
 )
 
 type commentUsecase struct {
@@ -13,12 +18,19 @@ func NewCommentRepository(cr domain.CommentRepository) *commentUsecase {
 	return &commentUsecase{commentRepository: cr}
 }
 
-func (s *commentUsecase) Create(input entity.CreateCommentInput) (domain.Comment, error) {
+func (s *commentUsecase) Create(input entity.CreateCommentInput, inputUri entity.CreateCommentUri) (domain.Comment, error) {
 	comment := domain.Comment{
 		Text:     input.Text,
-		SeriesId: int(input.SeriesId),
-		IsMovie:  input.IsMovie,
+		SeriesId: strconv.Itoa(inputUri.SeriesId),
 		UserId:   input.UserId,
+	}
+
+	if inputUri.Type == "movie" {
+		comment.IsMovie = true
+	} else if inputUri.Type == "tv" {
+		comment.IsMovie = false
+	} else {
+		return comment, domain.ErrBadRequest
 	}
 
 	newComment, err := s.commentRepository.Create(comment)
@@ -27,4 +39,54 @@ func (s *commentUsecase) Create(input entity.CreateCommentInput) (domain.Comment
 	}
 
 	return newComment, nil
+}
+
+func (s *commentUsecase) GetCommentByUserId(userId uint) ([]domain.Comment, error) {
+	var comments []domain.Comment
+	comments, err := s.commentRepository.GetByUserId(userId)
+	if err != nil {
+		return comments, domain.ErrInternalServerError
+	}
+
+	if comments[0].ID == 0 {
+		return comments, domain.ErrNotFound
+	}
+
+	var newComments []domain.Comment
+
+	for _, comment := range comments {
+		var url string
+		if comment.IsMovie {
+			url = "/movie/" + comment.SeriesId + "?api_key=6386ea8d1021b8ae5f21896f87ee2a09"
+		} else {
+			url = "/tv/" + comment.SeriesId + "?api_key=6386ea8d1021b8ae5f21896f87ee2a09"
+		}
+		fmt.Println(url)
+		res, err := http.Get("https://api.themoviedb.org/3" + url)
+		if err != nil {
+			return comments, err
+		}
+
+		resData, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return comments, err
+		}
+
+		if comment.IsMovie {
+			var movie entity.Movie
+			json.Unmarshal(resData, &movie)
+			comment.Series.Title = movie.OriginalTitle
+			comment.Series.Year = movie.ReleaseDate
+			comment.Series.ImagePoster = movie.PosterPath
+		} else {
+			var tv entity.Tv
+			json.Unmarshal(resData, &tv)
+			comment.Series.Title = tv.Name
+			comment.Series.Year = tv.FirstAirDate
+			comment.Series.ImagePoster = tv.PosterPath
+		}
+		newComments = append(newComments, comment)
+	}
+
+	return newComments, nil
 }
