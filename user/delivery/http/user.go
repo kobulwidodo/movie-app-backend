8869e8490008bb6952805dd2c3aed611efee714d
kobulwidodo <fadhilhan01@gmail.com>
2022-03-02 08:14:52 +0700

    rename folder util

--- user/delivery/http/user.go
+++ user/delivery/http/user.go
@@ -3,7 +3,7 @@ package http
 import (
 	"movie-app/domain"
 	"movie-app/user/entity"
-	"movie-app/util"
+	"movie-app/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,38 +23,38 @@ func NewUserHandler(r *gin.Engine, uu domain.UserUsecase, au domain.AuthUsecase)
 func (h *UserHandler) Register(c *gin.Context) {
 	var input entity.CreateUserInput
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, &util.Response{Message: err.Error()})
+		c.JSON(http.StatusUnprocessableEntity, &utils.Response{Message: err.Error()})
 		return
 	}
 
 	newUser, err := h.UserUsecase.Register(input)
 	if err != nil {
-		c.JSON(util.GetErrorCode(err), &util.Response{Message: err.Error()})
+		c.JSON(utils.GetErrorCode(err), &utils.Response{Message: err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusCreated, &util.Response{Data: &entity.GetUser{Name: newUser.Name, Email: newUser.Email, Bio: newUser.Bio}, Message: "successfully created a user"})
+	c.JSON(http.StatusCreated, &utils.Response{Data: &entity.GetUser{Name: newUser.Name, Email: newUser.Email, Bio: newUser.Bio}, Message: "successfully created a user"})
 }
 
 func (h *UserHandler) Login(c *gin.Context) {
 	var input entity.LoginInput
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, &util.Response{Message: err.Error()})
+		c.JSON(http.StatusUnprocessableEntity, &utils.Response{Message: err.Error()})
 		return
 	}
 
 	var userLogged domain.User
 	userLogged, err := h.UserUsecase.Login(input)
 	if err != nil {
-		c.JSON(util.GetErrorCode(err), &util.Response{Message: err.Error()})
+		c.JSON(utils.GetErrorCode(err), &utils.Response{Message: err.Error()})
 		return
 	}
 
 	token, err := h.AuthUsecase.GenerateToken(userLogged.ID)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, &util.Response{Message: domain.ErrInternalServerError.Error()})
+		c.JSON(http.StatusInternalServerError, &utils.Response{Message: domain.ErrInternalServerError.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, &util.Response{Data: &entity.GetUser{Name: userLogged.Name, Email: userLogged.Email, Bio: userLogged.Bio, Token: token}, Message: "Success Login"})
+	c.JSON(http.StatusOK, &utils.Response{Data: &entity.GetUser{Name: userLogged.Name, Email: userLogged.Email, Bio: userLogged.Bio, Token: token}, Message: "Success Login"})
 }
--- utils/response.go
+++ utils/response.go
@@ -1,4 +1,4 @@
-package util
+package utils
 
 type Response struct {
 	Data    interface{} `json:"data"`
