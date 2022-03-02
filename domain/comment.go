e29cf35b90c54801db11f8d7ced187d73f4c782b
kobulwidodo <fadhilhan01@gmail.com>
2022-03-02 15:01:05 +0700

    Add some method to interface and create new struct

--- domain/comment.go
+++ domain/comment.go
@@ -9,16 +9,25 @@ import (
 type Comment struct {
 	gorm.Model
 	Text     string `gorm:"type:text"`
-	SeriesId int
+	SeriesId string
 	IsMovie  bool
 	UserId   uint
 	User     User
+	Series   series `gorm:"-"`
+}
+
+type series struct {
+	Title       string
+	Year        string
+	ImagePoster string
 }
 
 type CommentRepository interface {
 	Create(comment Comment) (Comment, error)
+	GetByUserId(userId uint) ([]Comment, error)
 }
 
 type CommentUsecase interface {
-	Create(input _movieEntity.CreateCommentInput) (Comment, error)
+	Create(input _movieEntity.CreateCommentInput, inputUril _movieEntity.CreateCommentUri) (Comment, error)
+	GetCommentByUserId(userId uint) ([]Comment, error)
 }
