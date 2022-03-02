ef6a91eed47086b4691fe271bfb421b1eba94996
kobulwidodo <fadhilhan01@gmail.com>
2022-03-02 14:59:54 +0700

    Add some struct

--- comment/entity/entity.go
+++ comment/entity/entity.go
@@ -1,13 +1,33 @@
 package entity
 
 type CreateCommentInput struct {
-	Text     string `binding:"required"`
-	SeriesId uint
-	IsMovie  bool
-	UserId   uint
+	Text   string `binding:"required"`
+	UserId uint
 }
 
 type CreateCommentUri struct {
 	Type     string `uri:"type" binding:"required"`
 	SeriesId int    `uri:"seriesId" binding:"required"`
+}
+
+type GetCommentByIdUri struct {
+	Id int `uri:"id" binding:"required"`
+}
+
+type CommentOutput struct {
+	Id        int    `json:"id"`
+	Text      string `json:"text"`
+	CreatedAt string `json:"created_at"`
+}
+
+type Movie struct {
+	OriginalTitle string `json:"original_title"`
+	ReleaseDate   string `json:"release_date"`
+	PosterPath    string `json:"poster_path"`
+}
+
+type Tv struct {
+	Name         string `json:"name"`
+	FirstAirDate string `json:"first_air_date"`
+	PosterPath   string `json:"poster_path"`
 }
